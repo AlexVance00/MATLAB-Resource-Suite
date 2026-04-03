16b66cd75e3fbfe1b34357b5bd061fafb6721f98
AlexVance00 <alexandervance301@gmail.com>
2026-04-03 04:38:19 -0500

    compacting
    - universal getter, setter
    - copy constructor
    - now a handle class
    - removed redundant sprintf args in warning calls

--- Dev/aircraftClassDevScript.m
+++ Dev/aircraftClassDevScript.m
@@ -8,106 +8,56 @@ clc
 props = properties(Aircraft);
 num_props = length(props);
 
-%% Make getters
-bool_make_getters = false;
-% Template:
-
-        % function val = Get_(obj)
-        %     val = obj.val;
-        % end
-if bool_make_getters
-    functions = strings(1, num_props);
-    
-    for i_prop = 1:num_props
-        prop = props{i_prop};
-        function_expression = sprintf("\t\tfunction %s = Get_%s(obj)\n" + ...
-            "\t\t\t%s = obj.%s.value;\n" + ...
-            "\t\t\treturn;\n" + ...
-            "\t\tend\n", prop, prop, prop, prop);
-    
-        functions(i_prop) = function_expression;
-    end
-    
-    % Write out file for copy paste into class def file
-    filename_getters = "getters_copy_paste.txt";
-    delete(filename_getters)
-    writelines(functions, filename_getters);
-end
-
-%% Make setters
-bool_make_setters = false;
-% Template:
-
-        % function obj = Set_var(obj, val)
-	    %   obj.var = Aircraft.CheckInput(var, val);
-		% 	return;
-        % end
-if bool_make_setters
-    functions = strings(1, num_props);
-    
-    for i_prop = 1:num_props
-        prop = props{i_prop};
-        function_expression = sprintf("\t\tfunction obj = Set_%s(" + ...
-            "obj, val)\n" + ...
-            "\t\t\tobj.%s.value = Aircraft.CheckInput(""%s"", " + ...
-            "val);\n\t\t\treturn;\n\t\tend\n", prop, prop, prop);
-    
-        functions(i_prop) = function_expression;
-    end
-    
-    % Write out file for copy paste into class def file
-    filename_setters = "setters_copy_paste.txt";
-    delete(filename_setters)
-    writelines(functions, filename_setters);
-end
-
 %% Make equation permutations
+bool_make_equations = false;
 % Requires symbolic toolbox
-% Make master list of equations
-master_var_list = ["C_L"];
-num_vars = length(master_var_list);
-master_equation_list = ["C_L_0 + C_L_alpha * alpha_w + C_L_i_t * i_t + C_L_delta_e * delta_e;", "L / (q_bar_w * S_w);"];
-num_equations = length(master_equation_list);
-
-% Get all unique variables\
-required_vars = [];
-delimiters = ["+", "-", "*", "/", "^", "(", ")", ";"];
-for i_equation = 1:num_equations
-    equation = master_equation_list(i_equation);
-    equation = erase(equation, delimiters);
-    terms = strsplit(equation);
-    required_vars = unique([required_vars, terms]);
-end
-% Convert to symbolic
-syms(required_vars)
-
-% Convert equations to symbolic-friendly and remove semicolons
-master_equation_list = append(master_var_list, " == ", master_equation_list);
-erase(master_equation_list, ";")
-
-for i_var = 1:num_vars
-    equations = [C_L == C_L_0 + C_L_alpha * alpha_w + C_L_i_t * i_t + C_L_delta_e * delta_e, ...
-                 C_L == L / (q_bar_w * S_w)];
-end
-
-all_vars = [C_L, C_L_0, C_L_alpha, alpha_w, C_L_i_t, i_t, C_L_delta_e, delta_e, L, q_bar_w, S_w];
-
-output_lines = [];
-
-for i_eq = 1:length(equations)
-    eq = equations(i_eq);
-    vars_in_eq = symvar(eq);
-    for i_var = 1:length(vars_in_eq)
-        var = vars_in_eq(i_var);
-        rearranged = solve(eq, var);
-        var_name = string(var);
-        eq_string = string(rearranged);
-        line = var_name + " = " + eq_string + ";";
-        output_lines = [output_lines, line];
-        fprintf("%s\n", line);
+if bool_make_equations
+    % Make master list of equations
+    master_var_list = ["C_L"];
+    num_vars = length(master_var_list);
+    master_equation_list = ["C_L_0 + C_L_alpha * alpha_w + C_L_i_t * i_t + C_L_delta_e * delta_e;", "L / (q_bar_w * S_w);"];
+    num_equations = length(master_equation_list);
+    
+    % Get all unique variables\
+    required_vars = [];
+    delimiters = ["+", "-", "*", "/", "^", "(", ")", ";"];
+    for i_equation = 1:num_equations
+        equation = master_equation_list(i_equation);
+        equation = erase(equation, delimiters);
+        terms = strsplit(equation);
+        required_vars = unique([required_vars, terms]);
     end
+    % Convert to symbolic
+    syms(required_vars)
+    
+    % Convert equations to symbolic-friendly and remove semicolons
+    master_equation_list = append(master_var_list, " == ", master_equation_list);
+    erase(master_equation_list, ";")
+    
+    for i_var = 1:num_vars
+        equations = [C_L == C_L_0 + C_L_alpha * alpha_w + C_L_i_t * i_t + C_L_delta_e * delta_e, ...
+                     C_L == L / (q_bar_w * S_w)];
+    end
+    
+    all_vars = [C_L, C_L_0, C_L_alpha, alpha_w, C_L_i_t, i_t, C_L_delta_e, delta_e, L, q_bar_w, S_w];
+    
+    output_lines = [];
+    
+    for i_eq = 1:length(equations)
+        eq = equations(i_eq);
+        vars_in_eq = symvar(eq);
+        for i_var = 1:length(vars_in_eq)
+            var = vars_in_eq(i_var);
+            rearranged = solve(eq, var);
+            var_name = string(var);
+            eq_string = string(rearranged);
+            line = var_name + " = " + eq_string + ";";
+            output_lines = [output_lines, line];
+            fprintf("%s\n", line);
+        end
+    end
+    
+    writelines(output_lines, "permutations.txt");
 end
-
-writelines(output_lines, "permutations.txt");
 
 %% Test area
